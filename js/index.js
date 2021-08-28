755c44c4757880bddbdc97ee9f1ae5fdd855fc4d
ewa1do <eavera97@gmail.com>
2021-08-27 20:22:12 -0400

    implementing basic login

--- js/index.js
+++ js/index.js
@@ -1,1 +1,48 @@
 'use strict';
+
+const usersDB = [
+    {
+        username: 'admin',
+        password: 'admin',
+    },
+];
+
+
+const userInput = document.querySelector('#user');
+const passwordInput = document.querySelector('#pw');
+const btnSignIn = document.querySelector('#btn-signin');
+
+const mainWeb = document.querySelector('main');
+
+class UI { 
+    static hideWebPage () {
+        mainWeb.style.display = 'none';
+    }
+
+    static showWebPage () {
+        mainWeb.style.display = 'block';
+    }
+}
+
+class Login {
+
+    static isUserValid (username, password) {
+        usersDB.forEach(user => {
+            if (username === user.username && password === user.password) {
+                UI.showWebPage();
+            }
+        });
+        return false;
+    }
+
+}
+
+// Event handlers
+btnSignIn.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    Login.isUserValid(userInput.value, passwordInput.value);
+
+});
+
+document.addEventListener('DOMContentLoaded', UI.hideWebPage);
