8a460a17f7dcf06129ad0430f26ea482f0fa4d1c
ewa1do <eavera97@gmail.com>
2021-09-05 13:08:24 -0400

    creating the searchbar component

--- js/index.js
+++ js/index.js
@@ -4,36 +4,42 @@ const jerseysDB = [
     {
         name: 'Manchester United Jersey 2008-09',
         description: 'Cristiano Ronaldo Home Shirt #7 Season 2008-09',
+        league: 'Premier League',
         price: 79.99,
         url: 'https://dnre29p915wg3.cloudfront.net/media/tr:w-299,h-299,cm-pad_resize,bg-FFFFFF/catalog/product/m/a/man-united-07-home-boys-ronaldo-cp_2_2.jpg',
     },
     {
         name: 'Real Madrid Jersey 2016-17',
         description: 'Gareth Bale Home Shirt #11 Season 2016-17',
+        league: 'La Liga',
         price: 99.99,
         url: 'https://dnre29p915wg3.cloudfront.net/media/tr:w-299,h-299,cm-pad_resize,bg-FFFFFF/catalog/product/r/e/real-madrid-16-home-bale_1_2_3.jpg',
     },
     {
         name: 'FC Barcelona Jersey 2019-20',
         description: 'Leo Messi Home Shirt #10 Season 2019-20',
+        league: 'La Liga',
         price: 99.99,
         url: 'https://dnre29p915wg3.cloudfront.net/media/tr:w-299,h-299,cm-pad_resize,bg-FFFFFF/catalog/product/b/a/barcelona-19-home-special-weallplay-back_2_1.jpg',
     },
     {
         name: 'Borussia Dortmund Jersey 2016-17',
         description: 'Marco Reus Away Shirt #11 Season 2016-17',
+        league: 'Bundesliga',
         price: 69.99,
         url: 'https://dnre29p915wg3.cloudfront.net/media/tr:w-299,h-299,cm-pad_resize,bg-FFFFFF/catalog/product/d/o/dortmund-16-away-reus-new_1.jpg',
     },
     {
         name: 'AC Milan Jersey 2021-22',
         description: 'Zlatan Ibrahimovic Home Shirt #11 Season 2021-22',
+        league: 'Serie A',
         price: 99.99,
         url: 'https://media.nextopia.net/6d7ddad62cf3be5eb212c968adb23cda/0b54cd8c0eb2010f02da7b30a3772580.jpg?wm=0&h=299&w=299&bg=FFFFFF&eh=2&src=https%3A%2F%2Fdnre29p915wg3.cloudfront.net%2Fmedia%2Fcatalog%2Fproduct%2Fa%2Fc%2Fac-milan-21-home-ibrahimovic.jpg',
     },
     {
         name: 'Paris Saint-Germain Jersey 2021-22',
         description: 'Neymar JR Home Shirt #10 Season 2021-22',
+        league: 'League One',
         price: 99.99,
         url: 'https://media.nextopia.net/6d7ddad62cf3be5eb212c968adb23cda/1c9c518dd1f9cf05d8b3794cdfaa89fd.jpg?wm=0&h=299&w=299&bg=FFFFFF&eh=2&src=https%3A%2F%2Fdnre29p915wg3.cloudfront.net%2Fmedia%2Fcatalog%2Fproduct%2Fp%2Fs%2Fpsg-20-home-neymar.jpg',
     },
@@ -48,6 +54,8 @@ const btnSignIn = document.querySelector('#btn-signin');
 
 const mainContainerDiv = document.querySelector('.main-container');
 const mainWeb = document.querySelector('.main-web');
+
+const searchbar = document.getElementById('search-bar');
 
 
 // cart.html
@@ -67,7 +75,7 @@ class UI {
                 `
                     <div class="product"> 
                         <img src='${jersey.url}' class="product-img">
-                        <h5 class="product-desc">${jersey.description}</h5>
+                        <h5 class="product-desc"><span class="league">${jersey.league}</span> - ${jersey.description}</h5>
                         <span class="product-price">${jersey.price}</span>
                         <button class="product-cart">Add to Cart</button>
                     </div>
@@ -76,6 +84,22 @@ class UI {
                 mainContainerDiv.insertAdjacentHTML('beforeend', jerseyHTML);
             });
         }
+    }
+
+    static searchJersey () {
+        const filterValue = searchbar.value.toLowerCase();
+
+        const league = [...document.querySelectorAll('span.league')];
+    
+        league.forEach(league => {
+            const span = league.textContent.toLowerCase();
+            
+            if (span.indexOf(filterValue) > -1) {
+                league.parentElement.parentNode.style.display = '';
+            } else {
+                league.parentElement.parentNode.style.display = 'none';
+            }
+        });
     }
 
     static clearInputField (input) {
@@ -172,3 +196,4 @@ tableCart?.addEventListener('click', e => UI.removeCartItem(e));
 Cart.getJerseys();
 document.addEventListener('DOMContentLoaded', UI.displayJerseys);
 UI.displayCart();
+searchbar?.addEventListener('keyup', UI.searchJersey);
